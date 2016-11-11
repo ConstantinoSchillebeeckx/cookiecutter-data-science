9cbcda33ea5e74d28b12e5f92a976a565ccadba2
Constantino Schillebeeckx <constantino@matatu.com>
2016-11-11 12:05:57 -0600

    bug fix make data

--- {{cookiecutter.repo_name}}/src/data/make_dataset.py
+++ {{cookiecutter.repo_name}}/src/data/make_dataset.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+# will create a sym link of a biom table found within the given directory
+# specified by argv[1]
+# NOTE: will use the .biom table with longest name found in dir
+
+
 import sys, os
 
 # GLOBALS
-job_num = sys.argv[0]
+job_num = sys.argv[1]
 job_dir = '/home/data_repo/pre_processing/jobs/%s/stage_2/otupick/' %job_num
 
 # get OTU table with longest name
@@ -11,5 +16,4 @@ tables = [f for f in os.listdir(job_dir) if '.biom' in f]
 biom = sorted(tables)[-1]
 
 comm = "ln -s %s/%s data/raw/" %(job_dir, biom)
-
-print comm
+os.system(comm)
