d454b9328bea7a5cc07c598ec217abbfd4c713aa
Constantino Schillebeeckx <constantino@matatu.com>
2016-11-11 10:11:09 -0600

    hook copy OTU table to data dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,31 @@
-# Script is run after cookiecutter creates its cookie and will:
-# - create a symlink of OTU table if job_dir was specified
+#!/usr/bin/env python
+"""
 
-import sys
+Script is run after cookiecutter creates its cookie and will:
+- create a symlink of OTU table if job_dir was specified
 
+"""
 
-job_dir = '{{ cookiecutter.job_dir }}'
+import sys, os
+
+#################################################################################
+# GLOBALS                                                                       #
+#################################################################################
+job_num = '{{ cookiecutter.job_dir }}'
+job_dir = os.path.join('/home/data_repo/pre_processing/jobs/', job_num)
 repo = '{{ cookiecutter.module_name }}'
+repo_data_dir = os.path.join(repo, 'data/raw/')
 
+
+
+
+
+
+
+#################################################################################
+# OTU TABLE DATA                                                                #
+#################################################################################
 if job_dir.isdigit():
-    print "job_dir repo"
+    print "Copying OTU table from job %s to %s." %(job_dir, repo_data_dir)
+    comm = "ln -s /%s %s" %(job_dir, repo_data_dir)
+    os.system(comm)
