1d07476eb2323be4bbe43586995f28ca9f4af97a
Constantino Schillebeeckx <titous@gmail.com>
2016-11-18 16:25:24 -0600

    integration with mkdocs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -22,12 +22,14 @@ setup_git = True if '{{ cookiecutter.setup_git_repo }}' == "Yes" else False
 #################################################################################
 # NEXT STEPS                                                                    #
 #################################################################################
-print "\n\nCongrats! Your data analysis project directory has been created at: %s" %repo_name
+print "\n==----------------------------------------------------------------------------------------=="
+print "Congrats! Your data analysis project directory has been created at: %s" %repo_name
 
 if job_num.isdigit():
     print "\nSince you specified a job number (%s) you'll want to run 'make' from within the" %job_num
     print "project directory and use the command 'data' to setup your /data/raw files."
 
+print "==----------------------------------------------------------------------------------------==\n"
 
 
 #################################################################################
